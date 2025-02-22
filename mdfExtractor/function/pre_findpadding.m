85df3d98503d06994b165ede16d467a5ce850d5a
Aitchel17 <your.email@example.com>
2025-02-22 17:44:31 -0500

    now it's merged to class mdf

--- mdfExtractor/function/pre_findpadding.m
+++
@@ -1,8 +0,0 @@
-function [start_x,end_x] = pre_findpadding(frames)
-%% find padding caused by sinusoidal correction
-mean_x = mean(frames,[1,3]); % calculate mean value of y, z axis (y,x,z)
-tmp.nzloc = find(mean_x~=-2048); % find location of value not -2048
-start_x = tmp.nzloc(1); % start point of non zero
-end_x = tmp.nzloc(end); % end point of non zero
-end
-
