2409f7ff32c80c7c0cc0db78c26b5147343af5ab
Aitchel17 <your.email@example.com>
2025-02-22 17:52:37 -0500

    findpadding merged to protected method

--- mdfExtractor/class/mdf.m
+++ mdfExtractor/class/mdf.m
@@ -83,7 +83,7 @@ classdef mdf
     end
 
     methods (Access=protected, Static)
-        function [start_x,end_x] = pre_findpadding(frames)
+        function [start_x,end_x] = findpadding(frames)
             %% find padding caused by sinusoidal correction
             mean_x = mean(frames,[1,3]); % calculate mean value of y, z axis (y,x,z)
             tmp.nzloc = find(mean_x~=-2048); % find location of value not -2048
